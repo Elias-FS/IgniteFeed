387a2e98757e5920d7d6ae045acd5fb8953929de
Elias Fausto <elias-fausto@hotmail.com>
2023-06-21 23:02:08 -0300

    Implementando funções de Inserindo comentários, corrigindo as keys, comunicação entre componentes, removendo comentários, validando formulário e realizando likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,21 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function HandleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    })
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/Elias-FS.png" />
@@ -17,18 +30,18 @@ export function Comment() {
               </time>
             </div>
 
-            <button title="Deletar comentário">
-              <Trash size={24}/>
+            <button onClick={HandleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
